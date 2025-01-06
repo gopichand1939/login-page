0a83ad0caa08f4c59ff8c415e68ebb9d8fdf4770
gopichand <tummapalagopichand@gmail.com>
2025-01-06 21:50:33 +0530

    Replaced bcrypt with bcryptjs for Render compatibility

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs'); // Use bcryptjs instead of bcrypt
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const router = express.Router();
